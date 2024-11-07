fd1f139f94fba81774c769a1b85c4115a3081846
Gaurav Chauhan <182621594+1BH23CS024@users.noreply.github.com>
2024-11-08 04:48:33 +0530

    Major fixes

--- fcfs.c
+++ fcfs.c
@@ -4,54 +4,65 @@
 
 struct process_control_block
 {
-    int process_num, arrival_time, burst_time;
-    int completion_time, start_time, turn_around_time, waiting_time;
-} pcb[5] = {0}; // Maximum Processes Allowed = 5
+    int number, arrival_time, burst_time;
+    int start_time, waiting_time, completion_time, turn_around_time;
+} process[5], temp; // Maximum Processes Allowed = 5
 
 void main()
 {
-    int i, n;
-    int start_time[10], completion_time[10];
+    int n, start_time[10], completion_time[10];
     float total_wt = 0, total_tat = 0;
 
     printf("Number of Process = ");
     scanf("%d", &n);
 
-    for (i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
         printf("\nProcess Number = ");
-        scanf("%d", &pcb[i].process_num);
+        scanf("%d", &process[i].number);
 
         printf("\tArrival Time = ");
-        scanf("%d", &pcb[i].arrival_time);
+        scanf("%d", &process[i].arrival_time);
 
         printf("\tBurst Time = ");
-        scanf("%d", &pcb[i].burst_time);
+        scanf("%d", &process[i].burst_time);
+    }
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (process[i].arrival_time > process[j].arrival_time)
+            {
+                temp = process[i];
+                process[i] = process[j];
+                process[j] = temp;
+            }
+        }
     }
 
     printf("\nProcess Name\tArrival Time\tBurst Time\tStart Time\tCompletion Time\t\tWaiting Time\tTurn Around Time\n");
-    for (i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
-        if (i == 0)
-            pcb[i].start_time = pcb[i].arrival_time;
-        else
-            pcb[i].start_time = pcb[i - 1].completion_time;
+        // if i == 0 -> process[i].start_time = process[i].arrival_time
+        // else -> process[i].start_time = process[i - 1].completion_time
+        process[i].start_time = (i == 0) ? process[i].arrival_time : process[i - 1].completion_time;
 
-        pcb[i].waiting_time = pcb[i].start_time - pcb[i].arrival_time;          // Waiting Time = Start Time - Arrival Time
-        pcb[i].completion_time = pcb[i].start_time + pcb[i].burst_time;         // Completion Time = Start Time + Burst Time
-        pcb[i].turn_around_time = pcb[i].completion_time - pcb[i].arrival_time; // Turn Around Time = Completion Time - Arrival Time
+        process[i].completion_time = process[i].start_time + process[i].burst_time;
+        process[i].turn_around_time = process[i].completion_time - process[i].arrival_time;
+        process[i].waiting_time = process[i].turn_around_time - process[i].burst_time;
 
         printf("%d\t\t%d\t\t%d\t\t%d\t\t%d\t\t\t%d\t\t%d\n",
-               pcb[i].process_num,
-               pcb[i].arrival_time,
-               pcb[i].burst_time,
-               pcb[i].start_time,
-               pcb[i].completion_time,
-               pcb[i].waiting_time,
-               pcb[i].turn_around_time);
+               process[i].number,
+               process[i].arrival_time,
+               process[i].burst_time,
+               process[i].start_time,
+               process[i].completion_time,
+               process[i].waiting_time,
+               process[i].turn_around_time);
 
-        total_wt += pcb[i].waiting_time;
-        total_tat += pcb[i].turn_around_time;
+        total_wt += process[i].waiting_time;
+        total_tat += process[i].turn_around_time;
     }
 
     printf("\nAverage Waiting Time = %f\n", total_wt / n);
